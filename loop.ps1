4b111c998abedcb0a3443261d9b0324a12a96029
RafaelKC <rafaelkaua97@gmail.com>
2024-06-10 22:46:20 -0300

    Ajustes

--- loop.ps1
+++ loop.ps1
@@ -1,17 +1,22 @@
-$csvPath = "./out/output.csv"
+param (
+    [string]$nome,
+    [int]$totalLoops
+)
+
+$csvPath = "./out/"+$nome+".csv"
 
 $headers = "N_threads,MS
 $headers | Out-File -FilePath $csvPath -Encoding utf8"
 
-for ($i = 1; $i -le 20; $i++) {
+for ($i = 1; $i -le $totalLoops; $i++) {
     $time = java ".\src\Main.java" $i
-    Write-Output $rsult
 
     $data = [PSCustomObject]@{
         MS = $time
         N_threads = $i
     }
     $data | Export-Csv -Path $csvPath -Append -NoTypeInformation
-    Write-Output "-------------------------------"
+    
     Write-Output $time
+    Write-Output "-------------------------------"
 }
